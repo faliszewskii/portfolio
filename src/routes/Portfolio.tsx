0a89e6f7ac02c552c07fa0f4d24efcd13bc2517d
faliszewskii <igor.faliszewski1@gmail.com>
2023-03-06 23:06:27 +0100

    Test DB connection

--- src/routes/Portfolio.tsx
+++ src/routes/Portfolio.tsx
@@ -1,8 +1,41 @@
+import {useEffect, useState} from "react";
 
-function Portfolio() {
+export interface Project {
+    _id: string,
+    title: string,
+    description: string,
+    repository: string
+}
+
+const Portfolio = () => {
+    const [projects, setProjects] = useState<null|Project[]>(null)
+    useEffect(() => {
+        fetch('https://personal-825b.restdb.io/rest/projects', {
+            method: 'GET',
+            headers: {
+                'x-apikey': `${process.env.DB_API_KEY}`
+            }
+        })
+            .then((res) => {
+                if(!res.ok) return;
+                res.json().then((project)=> setProjects(project));
+            });
+    })
     return (
         <>
-            Hello Portfolio
+            <div>Hello Portfolio</div>
+            <div style={{border: '3px black'}}>
+                { projects !== null ?
+                    projects.map(project =>
+                        <div>
+                            <h1>{project.title}</h1>
+                            <div>{project.repository}</div>
+                            <div>{project.description}</div>
+                        </div>
+                    ):
+                    <div>No projects</div>
+                }
+            </div>
         </>
     );
 }
